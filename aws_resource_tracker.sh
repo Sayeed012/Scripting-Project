06f6127b93937a37fc8b86a1232850dfc33d72f1
Sayeed012 <126281355+Sayeed012@users.noreply.github.com>
2024-10-18 15:56:55 +0530

    Create aws_resource_tracker.sh

---
+++ aws_resource_tracker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+######################
+# Author: Sayeed
+# Date: 18th-Oct
+#
+# Version: v1
+#
+# This script will report simplified AWS resource usage
+######################
+
+set -x
+
+# Clear the previous content of resourceTracker (optional)
+> resourceTracker
+
+# AWS S3 Buckets
+echo "List of S3 Buckets:" | tee -a resourceTracker
+aws s3 ls | awk '{print $3}' | tee -a resourceTracker
+
+# AWS EC2 Instances
+echo "List of EC2 Instance IDs and States:" | tee -a resourceTracker
+aws ec2 describe-instances | jq -r '.Reservations[].Instances[] | {InstanceId, State: .State.Name}' | tee -a resourceTracker
+
+# AWS Lambda Functions
+echo "List of Lambda Function Names:" | tee -a resourceTracker
+aws lambda list-functions | jq -r '.Functions[].FunctionName' | tee -a resourceTracker
+
+# AWS IAM Users
+echo "List of IAM User Names:" | tee -a resourceTracker
+aws iam list-users | jq -r '.Users[].UserName' | tee -a resourceTracker
